bfcd3f8b7b5a26afdadada10ac4247ffbbf7e961
marciogama <marciogama2000@gmail.com>
2021-01-05 23:31:17 -0300

    Criação dos esqueletos CarroDaoJDBC e ClienteDaoJDBC

---
+++ src/model/dao/CarroDaoJDBC.java
@@ -0,0 +1,39 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Carro;
+
+public class CarroDaoJDBC implements CarroDao {
+
+	@Override
+	public void insert(Carro obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Carro obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Carro findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Carro> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/model/dao/ClienteDaoJDBC.java
@@ -0,0 +1,39 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Cliente;
+
+public class ClienteDaoJDBC implements ClienteDao {
+
+	@Override
+	public void insert(Cliente obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Cliente obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(String cpf) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Cliente findById(String cpf) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Cliente> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
